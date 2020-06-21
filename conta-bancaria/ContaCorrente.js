84d67a9f1585f987d5cd271a75bb73b56403b98d
Lucca Hora <luccahora@gmail.com>
2020-06-21 19:35:25 -0300

    Contador de contas

--- conta-bancaria/ContaCorrente.js
+++ conta-bancaria/ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente { 
+    static numeroDeContas = 0;
     agencia;
     _cliente;
 
@@ -22,6 +23,7 @@ export class ContaCorrente {
     constructor(cliente, agencia){
         this.agencia = agencia;
         this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
     }
 
     sacar(valorSacar) {
